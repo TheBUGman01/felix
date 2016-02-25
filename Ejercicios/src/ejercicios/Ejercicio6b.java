5ac5e276e90e1b750bd46941105e6c6dcdcef018
Key2Board12 <captaincoca365@gmail.com>
2016-02-25 18:35:50 +0100

    Per si las moscas
    Ejercicio 6b

--- Ejercicios/src/ejercicios/Ejercicio6b.java
+++ Ejercicios/src/ejercicios/Ejercicio6b.java
@@ -2,13 +2,11 @@ package ejercicios;
 
 public class Ejercicio6b {
 	public static void main (String [] args){
-		int i = 99;
+		int i = 100;
 		
 		while (i > 1){
 			System.out.println(i);
-			i--;
-			i--;
+			i= i-2;
 		}
-		
 	}
 }
---
+++ Ejercicios/src/ejercicios/Lib.java
@@ -0,0 +1,9 @@
+package ejercicios;
+
+public class Lib {
+	public static void main (String [] args){
+	
+	
+	
+	}
+}
